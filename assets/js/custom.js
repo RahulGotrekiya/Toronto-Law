557e8c23a7c8ae4574b5581180abf9dc2c5ac1c3
Rahul Gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com>
2024-06-15 21:19:32 +0530

    Add Js for testimonials-slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,9 @@
 let hamburger = document.querySelector(".hamburger");
-let nav = document.querySelector(".header-menu");
+let nav = document.querySelector(".header-menu")
 let white = document.querySelector(".header-info");
 
-hamburger.addEventListener("click", function () {
+
+hamburger.addEventListener("click", function() {
   hamburger.classList.toggle("isactive");
   nav.classList.toggle("active");
   white.classList.toggle("active");
@@ -20,8 +21,43 @@ $(".video").magnificPopup({
       "</div>",
   },
   callbacks: {
-    markupParse: function (template, values, item) {
+    markupParse: function(template, values, item) {
       values.title = item.el.attr("title");
     },
   },
 });
+
+// Testimonial slides
+const slider = document.querySelector(".slider-overflow");
+const slidesContainer = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+const leftArrow = document.querySelector(".left");
+const rightArrow = document.querySelector(".right");
+
+let slideWidth = slides[0].offsetWidth;
+let slideIndex = 0;
+
+function slideTo(index) {
+  const newIndex = index % slides.length;
+
+  const nextSlide = newIndex < 0 ? slides.length - 1 : newIndex;
+
+  // Slide to the next slide
+  slidesContainer.style.transform = `translateX(${-nextSlide * slideWidth}px)`;
+  slideIndex = nextSlide;
+}
+
+leftArrow.addEventListener('click', function() {
+  slideTo(slideIndex - 1);
+});
+
+rightArrow.addEventListener('click', function() {
+  slideTo(slideIndex + 1);
+});
+
+// Optional: Auto slide infinitely
+//function autoSlide() {
+//  slideTo(slideIndex + 1);
+//}
+//
+//setInterval(autoSlide, 3000); 
